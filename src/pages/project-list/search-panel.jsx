3e19e972dd841a5c5bb31adb8dc30f0e3cdfba6e
William Ji <williamji12345@gmail.com>
2022-11-01 05:08:07 -0400

    Initial commit

---
+++ src/pages/project-list/search-panel.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export const SearchPanel = ({ param, setParam }) => {
+    const [users, setUsers] = useState([])
+
+    return <form>
+        <div>
+            <input type="text" value={param.name} onChange={evt => setParam({
+                ...param,
+                name: evt.target.value
+            })} />
+            <select value={param.personId} onChange={evt => setParam({
+                ...param,
+                personId: evt.target.value
+            })}>
+                <option value={''}>Leads</option>
+                {
+                    users.map(user => <option value={user.id}>{user.name}</option>)
+                }
+            </select>
+        </div>
+    </form>
+}
